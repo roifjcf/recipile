2bc69df124403534f364bd60384daa4b7fdfc70b
roifjcf <roifjcf@google.com>
2025-08-01 14:08:50 +1000

    fix: remove cached files and apply .gitignore

--- frontend/src/contexts/pushNotificationContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-interface contextInterface {
-  addNotificationMessage?: (hookval: string) => void,
-};
-
-const PushNotificationContext = createContext<contextInterface | undefined>(undefined);
-export default PushNotificationContext;
